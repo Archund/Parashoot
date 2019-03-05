4dc5b3f74bc14841196945e65e8db6b51e91d705
Archund <jarchund@gmail.com>
2019-03-05 14:45:57 -0500

    Fixed sky

---
+++ scripts/Baseman.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+var powerlevel = 0
+
+func _ready():
+	powerlevel = 0
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
